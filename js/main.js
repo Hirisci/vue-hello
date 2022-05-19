302956754ea5f24d1c3d72f945227b50023bdb60
Hirisci <www.alan92@icloud.com>
2022-05-19 14:01:42 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,12 @@
+let app = new Vue(
+    {
+        el: '#app',
+        data: {
+            message: "hello Vue!",
+            image: {
+                img: "https://cdn.icon-icons.com/icons2/2415/PNG/512/vuejs_original_wordmark_logo_icon_146305.png",
+                nome: "logo Vue js",
+            }
+        }
+    }
+)
